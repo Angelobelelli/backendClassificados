7732b5321de8a2819f09893bec4d9f4b8fe95613
Angelo Belelli Bugni <130174985+Angelobelelli@users.noreply.github.com>
2025-05-20 10:16:24 -0300

    retirando os arquivos do typescript para começar a usa o node.js

--- src/routes/productRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance } from 'fastify';
-import { ProductController } from '../controllers/ProductController';
-
-export async function productRoutes(fastify: FastifyInstance) {
-  fastify.get('/products', ProductController.index);
-  fastify.get('/products/:id', ProductController.show);
-  fastify.post('/products', ProductController.store);
-  fastify.delete('/products/:id', ProductController.destroy);
-}
